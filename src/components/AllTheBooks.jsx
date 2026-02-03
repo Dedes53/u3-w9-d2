51398af7ff3a308b6b3174c9ba19f798020301bd
Dedes <federico.lepore73@gmail.com>
2026-02-03 17:28:11 +0100

    AllThe Books col

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -3,6 +3,7 @@ import fantasy from '../books/fantasy.json';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 class AllTheBooks extends Component {
     render() {
@@ -10,16 +11,20 @@ class AllTheBooks extends Component {
         return (
 
             <Container className="my-4">
-                <Row>
+                <Row className="g-4">
                     {fantasy.map((book) => (
-                        <Card key={book.asin} style={{ width: '18rem' }} className="col-12 col-md-8 col-lg-6 col mb-3">
-                            <Card.Img variant="top" src={book.img} />
-                            <Card.Body>
-                                <Card.Title>{book.title}</Card.Title>
-                                <Card.Text>€{book.price}</Card.Text>
-                                <Card.Text>{book.category}</Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Col xs={12} md={6} lg={4} xl={3} key={book.asin}>
+                            <Card className="mb-3" >
+                                <Card.Img fluid={true} variant="top" src={book.img} className="object-fit-cover" />
+                                <Card.Body>
+                                    <Card.Title>{book.title}</Card.Title>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <Card.Text className="m-0">{book.category}</Card.Text>
+                                        <Card.Text>€{book.price}</Card.Text>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     ))}
                 </Row>
             </Container>
