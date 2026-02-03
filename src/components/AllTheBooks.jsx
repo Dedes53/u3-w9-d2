4ed8cb71e9aa133ffb8f4e51d65b25bc478f4455
Dedes <federico.lepore73@gmail.com>
2026-02-03 17:06:29 +0100

    booksmap

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,13 +1,32 @@
 import { Component } from 'react';
+import fantasy from '../books/fantasy.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 
 class AllTheBooks extends Component {
-
     render() {
-        return (
-            
-        )
-    }
 
+        return (
+
+            <Container className="my-4">
+                <Row>
+                    {fantasy.map((book) => (
+                        <Card key={book.asin} style={{ width: '18rem' }} className="col-12 col-md-8 col-lg-6 col mb-3">
+                            <Card.Img variant="top" src={book.img} />
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>€{book.price}</Card.Text>
+                                <Card.Text>{book.category}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </Row>
+            </Container>
+
+        );
+
+    }
 }
 
 export default AllTheBooks;
