2f1fc95fc78021fa8fbd9c0243903d64a61995ce
Dedes <federico.lepore73@gmail.com>
2026-02-04 14:55:08 +0100

    SingleBook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,17 +1,19 @@
 import { Component } from "react";
+import { Card } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
 
 class SingleBook extends Component {
-    render(props) {
+    render() {
 
         return (
-            <Col xs={12} md={6} lg={4} xl={3} key={props.asin}>
+            <Col xs={12} md={6} lg={4} xl={3} key={this.props.asin}>
                 <Card className="mb-3" >
-                    <Card.Img variant="top" src={props.img} className="object-fit-cover" />
+                    <Card.Img variant="top" src={this.props.img} className="object-fit-cover" />
                     <Card.Body>
-                        <Card.Title>{props.title}</Card.Title>
+                        <Card.Title>{this.props.title}</Card.Title>
                         <div className="d-flex justify-content-between align-items-center">
-                            <Card.Text className="m-0">{props.category}</Card.Text>
-                            <Card.Text>€{props.price}</Card.Text>
+                            <Card.Text className="m-0">{this.props.category}</Card.Text>
+                            <Card.Text>€{this.props.price}</Card.Text>
                         </div>
                     </Card.Body>
                 </Card>
