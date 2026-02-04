3f03d157deac0b6341e740be2166604c60ca1c80
Dedes <federico.lepore73@gmail.com>
2026-02-04 15:09:14 +0100

    state create

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -3,11 +3,18 @@ import { Card } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 
 class SingleBook extends Component {
+
+    state = {
+        seleced: false
+    }
+
     render() {
         // in una class le props non si possono passare, quindi bisogna usare this.props.esempio
         // altriementi si può utilizzare il destructuring all'inizio di render()
         //ES: const { img, title, category, price } = this.props;
         //a questo punto nel mio codice posso usare direttamente img, title, category, price senza this.props tra {} 
+
+
         return (
             <Col xs={12} md={6} lg={4} xl={3} key={this.props.asin}>
                 <Card className="mb-3" >
