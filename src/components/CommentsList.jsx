c9bbf6a19a00872b87df5fb6226358b01dd7f8a9
Dedes <federico.lepore73@gmail.com>
2026-02-05 15:15:11 +0100

    commentList

---
+++ src/components/CommentsList.jsx
@@ -0,0 +1,24 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+// componente lista che racchiude tutti i commenti di un libro contenuta in CommentArea.
+// Questo componente riceve in props un array di commenti e li mapppa generanto tanti ListGroup.Item quanti sono i commenti nell'array
+const CommentsList = (props) => {
+    return (
+        <ListGroup>
+            {props.comments.map((comm) =>
+                <ListGroup.Item key={comm.elementId}>
+                    <p>{comm.comment}</p>
+                </ListGroup.Item>
+            )}
+        </ListGroup>
+    )
+}
+
+export default CommentsList;
+
+
+
+
+
+
+
